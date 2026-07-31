455e357b92fc07f2bdc9e6891320142f9c326a5e
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2026-07-30 21:59:13 -0300

    clean project

--- resources/views/main.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.main_layout')
-
-@section('content')
-<h1>View Main</h1>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,3 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 
-// Route::get('/', [MainController::class, 'index']);
-Route::get('/main/{value}', [MainController::class, 'index']);
-Route::get('/page2/{value}', [MainController::class, 'page2']);
