0cb081be9a6b8d7cea69e535e06b3c4a5275f266
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2026-08-02 22:11:02 -0300

    login e logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -30,11 +32,35 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        echo 'OK';
+        $user = User::where('username', $username)
+            ->where('deleted_at', NULL)
+            ->first();
+
+        if (!$user) {
+            return redirect()->back()->withInput()->with('loginError', 'Username ou password incorretos.');
+        }
+
+        if (!password_verify($password, $user->password)) {
+            return redirect()->back()->withInput()->with('loginError', 'Username ou password incorretos.');
+        }
+
+        // Update last_login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        session([
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username
+            ]
+        ]);
+
+        echo 'Logado com sucesso!';
     }
 
     public function logout()
     {
-
+        session()->forget('user');
+        return redirect()->to('/login');
     }
 }
