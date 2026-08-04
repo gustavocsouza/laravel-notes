aad4f95e72687afa80ac7104cd0335079ad6c763
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2026-08-03 21:58:01 -0300

    added auth middlewares

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-   
+   public function index() 
+   {
+        echo 'Main Controller';
+   }
+
+   public function newNote() 
+   {
+        echo 'New Note';
+   }
 }
