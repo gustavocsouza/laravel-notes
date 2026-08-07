bbd23f5b7e53213c83c9c1bead00c8e7ec798578
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2026-08-06 23:44:37 -0300

    added edit and delete routes, added Crypt methods

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,28 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
-   public function index() 
-   {
-     // todo -> load users
-     $userId = session('user.id');
-     $user = User::find($userId)->toArray();
-     $notes = User::find($userId)->notes()->get()->toArray();
+  public function index()
+  {
+    // todo -> load users
+    $userId = session('user.id');
+    $notes = User::find($userId)->notes()->get()->toArray();
 
-     echo '<pre>';
-     print_r($user);
-     print_r($notes);
+    // show home view
+    return view('home', ['notes' => $notes]);
+  }
 
-     die();
-     // show home view
-     return view('home');
-   }
+  public function newNote()
+  {
+    echo 'New Note';
+  }
 
-   public function newNote() 
-   {
-        echo 'New Note';
-   }
+  public function editNote($id) 
+  {
+    try {
+      $id = Crypt::decrypt($id);
+    } catch (DecryptException $e) {
+      return redirect()->route('home');
+    }
+
+    echo $id;
+  }
+
+  public function deleteNote($id) 
+  {
+    try {
+      $id = Crypt::decrypt($id);
+    } catch (DecryptException $e) {
+      return redirect()->route('home');
+    }
+
+    echo $id;
+  }
 }
