ff191ad61e23849c1b46f8f3232ec4fe3d244d28
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2026-08-16 21:38:14 -0300

    delete note

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
-use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
@@ -15,7 +13,11 @@ class MainController extends Controller
   {
     // todo -> load users
     $userId = session('user.id');
-    $notes = User::find($userId)->notes()->get()->toArray();
+    $notes = User::find($userId)
+                ->notes()
+                ->whereNull('deleted_at')
+                ->get()
+                ->toArray();
 
     // show home view
     return view('home', ['notes' => $notes]);
@@ -108,6 +110,31 @@ class MainController extends Controller
   {
     $id = Operations::decryptId($id);
 
+    // load note
+    $note = Note::find($id);
+
+    // show delete note confirm
+    return view('delete_note', ['note' => $note]);    
+  }
+
+  public function deleteNoteConfirm($id) 
+  {
+    // check if $id is encrypted
+    $id = Operations::decryptId($id);
+
+    // load note
+    $note = Note::find($id);
+
+    // 1.hard delete
+    // $note->delete();
+
+    // 2.soft delete
+    $note->deleted_at = date('Y:m:d H:i:s');
+    $note->save();
+
+
+    // redirect to home
+    return redirect()->route('home');
     echo $id;
   }
 }
